3d235e64aad47542db930e0f1cceafe30f06a891
Quiyet Brul <qbrul0514@gmail.com>
2021-07-10 12:58:55 -0700

    Update Handler.java
    - removed screenClear class member -- turns out to be redundant

--- src/com/quiyetbrul/breakout/Handler.java
+++ src/com/quiyetbrul/breakout/Handler.java
@@ -24,17 +24,4 @@ public class Handler {
 
     public void addObject(GameObject object) {this.object.add(object);}
     public void removeObject(GameObject object) {this.object.remove(object);}
-
-    public void ScreenClear() { // clears the screen and rerenders player
-        for (int i = 0; i < object.size(); i++) {
-            GameObject tempObject = object.get(i);
-
-            if (tempObject.getId() == ID.Player) {
-                object.clear();
-                if(GameMain.gameStart != GameMain.GAME_STATE.GameOver){ 
-                    addObject(new Player((int) tempObject.getX(), (int) tempObject.getY(), ID.Player, this));
-                }
-            }
-        }
-    }
 }
