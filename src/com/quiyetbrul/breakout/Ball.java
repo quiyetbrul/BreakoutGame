12b7b9e5888aecd28bcbba06a99b253b8012002a
Quiyet Brul <qbrul0514@gmail.com>
2021-07-10 12:56:44 -0700

    Update Ball.java
    - created variables for ball size
    - ball bounces on LTD edges
    - loses life when ball goes beyond bottom edge
    ;
    NEED TO IMPLEMENT
    - ball bounces on paddle

--- src/com/quiyetbrul/breakout/Ball.java
+++ src/com/quiyetbrul/breakout/Ball.java
@@ -1,10 +1,14 @@
 package com.quiyetbrul.breakout;
 
+import static com.quiyetbrul.breakout.GameMain.*;
+
 import java.awt.*;
 
 public class Ball extends GameObject{
 
     private Handler handler;
+    private HUD hud;
+
     private final int BALL_WIDTH = 10;
     private final int BALL_HEIGHT = 10;
 
@@ -13,18 +17,12 @@ public class Ball extends GameObject{
 
         this.handler = handler;
 
-//        setVelX(5); setVelY(5);
-        velX = 5; velY = 5;
+        setVelX(5); setVelY(5);
     }
 
     public Rectangle getBounds(){
-        return new Rectangle((int)x, (int)y, BALL_WIDTH, BALL_HEIGHT);
+        return new Rectangle((int) x_coordinate, (int) y_coordinate, BALL_WIDTH, BALL_HEIGHT);
     }
-
-
-    //// LOSE LIFE
-    // ball x-coordinate < 0 && y-coodrinate > WINDOW_HEIGTH || x-coordinate > WINDOW_WIDTH && y-coodrinate > WINDOW_HEIGTH
-    // LIVES-=1;
 
     //// BALL BOUNCE
     // ball x-coordinate == PADDLE_X && y-coordinate = PADDLE_Y
@@ -33,20 +31,40 @@ public class Ball extends GameObject{
     @Override
     public void tick() {
         // updates x,y coordinates in accordance to velocity
-        x += velX;
-        y += velY;
+        x_coordinate += getVelX();
+        y_coordinate += getVelY();
 
-        // bounce on ALL FOUR WALLS for now
-        if(y<=0 || y >= GameMain.WINDOW_HEIGHT - 50) velY *= -1;
-        if(x<=0 || x >= GameMain.WINDOW_WIDTH - 20) velX *= -1;
+        // bounce on LTR edge for now
+        if(y_coordinate <=0) velY *= -1;
+        if(x_coordinate <=0 || x_coordinate >= GameMain.WINDOW_WIDTH - 20) velX *= -1;
 
-        // add bounce on paddle
-
-//        handler.addObject(new RenderObject((int)x, (int)y, ID.Ball, Color.WHITE, BALL_WIDTH, BALL_HEIGHT, handler));
+        // each time ball goes beyond bottom edge, ball gets destroyed
+        // lives > 0 ? add ball
+        if(y_coordinate >= WINDOW_HEIGHT) {
+            hud.LIVES -= 1;
+            handler.removeObject(this);
+            if(hud.LIVES > 0)
+                handler.addObject(new Ball(BALL_X, BALL_Y, ID.Ball, handler));
+            if(gameStart == GAME_STATE.GameOver)
+                handler.removeObject(this);
+        }
+//        collision();
     }
+
+//    private void collision(){
+//        for(int i = 0; i < handler.object.size(); i++){
+//            GameObject player = handler.object.get(i);
+//
+//            if(player.getId() == ID.Ball){
+//                if(getBounds().intersects(player.getBounds())){
+//                    HUD.HEALTH -= 2;
+//                }
+//            }
+//        }
+//    }
 
     @Override
     public void render(Graphics g) {
-        g.fillOval((int)x, (int)y, BALL_WIDTH, BALL_HEIGHT);
+        g.fillOval((int) x_coordinate, (int) y_coordinate, BALL_WIDTH, BALL_HEIGHT);
     }
 }
