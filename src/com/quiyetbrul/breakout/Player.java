72cb8259b097bde9242f6750764742b3d1a49f53
Quiyet Brul <qbrul0514@gmail.com>
2021-07-09 23:30:17 -0700

    Update Player.java

--- src/com/quiyetbrul/breakout/Player.java
+++ src/com/quiyetbrul/breakout/Player.java
@@ -7,8 +7,8 @@ import java.awt.*;
 public class Player extends GameObject {
 
     Handler handler;
-    private final int PADDLE_WIDTH = 120;
-    private final int PADDLE_HEIGHT = 30;
+    private final int PADDLE_WIDTH = 100;
+    private final int PADDLE_HEIGHT = 10;
 
     public Player(int x, int y, ID id, Handler handler) {
         super(x, y, id);
@@ -25,28 +25,9 @@ public class Player extends GameObject {
 
         // x-coordinate boundary
         x = GameMain.clamp((int)x, 0, GameMain.WINDOW_WIDTH - PADDLE_WIDTH);
-        
-        //player character
-        handler.addObject(new RenderObject((int)x, 0, ID.Player, Color.white, PADDLE_X, PADDLE_Y, handler));
-
-//        collision();
     }
 
-    // ball y-coordinate < y -> LIVES-=1
-//    private void collision(){
-//        for(int i = 0; i < handler.object.size(); i++){
-//            GameObject player = handler.object.get(i);
-//
-//            if(player.getId() == ID.EnemyBasic
-//                    || player.getId() == ID.EnemyFast
-//                    || player.getId() == ID.EnemySmart
-//                    || player.getId() == ID.EnemyBoss){
-//                if(getBounds().intersects(player.getBounds())){
-//                    HUD.LIVES -= 1;
-//                }
-//            }
-//        }
-//    }
+
 
     public void render(Graphics g) {
         // renders paddle
