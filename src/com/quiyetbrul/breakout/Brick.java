b714e6ee9df42dcc10389798184f89da5fe41e4b
Quiyet Brul <qbrul0514@gmail.com>
2021-07-11 00:23:12 -0700

    Update Brick.java

--- src/com/quiyetbrul/breakout/Brick.java
+++ src/com/quiyetbrul/breakout/Brick.java
@@ -11,20 +11,24 @@ public class Brick extends GameObject{
 
     public Brick(int x_coordinate, int y_coordinate, ID id, Handler handler) {
         super(x_coordinate, y_coordinate, id);
-    }
 
-    @Override
-    public void tick() {
-
-    }
-
-    @Override
-    public void render(Graphics g) {
-
+        this.handler = handler;
     }
 
     @Override
     public Rectangle getBounds() {
         return null;
+    }
+
+    @Override
+    public void tick() {
+        // for (int i = 5; i < WINDOW_WIDTH -5; i++)
+        // add each brick
+        //
+    }
+
+    @Override
+    public void render(Graphics g) {
+
     }
 }
