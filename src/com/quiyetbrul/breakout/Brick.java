5cf06dd2db87b144bfd2041cb7d85c5d1a890315
Quiyet Brul <qbrul0514@gmail.com>
2021-07-11 14:59:50 -0700

    Update Brick.java
    - brick class

--- src/com/quiyetbrul/breakout/Brick.java
+++ src/com/quiyetbrul/breakout/Brick.java
@@ -2,6 +2,8 @@ package com.quiyetbrul.breakout;
 
 import java.awt.*;
 
+import static com.quiyetbrul.breakout.GameMain.*;
+
 public class Brick extends GameObject{
 
 
@@ -9,26 +11,32 @@ public class Brick extends GameObject{
     private Player player;
     private HUD hud;
 
+    public final int BRICK_WIDTH = 50;
+    public final int BRICK_HEIGHT = 20;
+
+    public static int BRICK_X = 15;
+    public static int BRICK_Y = 15;
+
     public Brick(int x_coordinate, int y_coordinate, ID id, Handler handler) {
         super(x_coordinate, y_coordinate, id);
 
         this.handler = handler;
     }
 
-    @Override
     public Rectangle getBounds() {
-        return null;
+        return new Rectangle((int)x_coordinate, (int)y_coordinate, BRICK_WIDTH, BRICK_HEIGHT);
     }
 
     @Override
     public void tick() {
-        // for (int i = 5; i < WINDOW_WIDTH -5; i++)
-        // add each brick
-        //
+        if(gameStart == GameMain.GAME_STATE.GameOver) {
+            handler.removeObject(this);
+        }
     }
 
     @Override
     public void render(Graphics g) {
-
+        g.setColor(Color.BLUE);
+        g.fillRect((int) x_coordinate, (int) y_coordinate, BRICK_WIDTH, BRICK_HEIGHT);
     }
 }
